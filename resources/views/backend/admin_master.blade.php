498343e11a513cd4cecdb945be8087fea1d32093
sumon sarker <csesumonpro@gmail.com>
2018-06-01 00:55:14 +0600

    admin panel init

---
+++ resources/views/backend/admin_master.blade.php
@@ -0,0 +1,4 @@
+
+@include('backend.includes.header')
+@yield('admin_content')
+@include('backend.includes.footer')
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('backend/dashboard');
 });
+
+
+
 
