0a3df12396c2c95350ed01e64b35357559b0f2cb
sumon sarker <csesumonpro@gmail.com>
2018-06-01 16:30:27 +0600

    add Categories and Sub Categories Table

---
+++ database/migrations/2018_06_01_102032_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cat_title')->nullable();
+            $table->string('cat_name')->nullable();
+            $table->string('cat_image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
